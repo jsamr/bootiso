47a62683420dcca79db77ca1b190402789df3b6e
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-05-16 09:45:09 -0300

    completions: add GPLv3 preamble

--- extra/completions/completions.bash
+++ extra/completions/completions.bash
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+#
+# bootiso - create a bootable USB drive from an image file
+# Copyright (C) 2018-2020 jules randolph <jules.sam.randolph@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __bootiso_debug() {
   if [[ -n $BASH_COMP_DEBUG_FILE ]]; then
