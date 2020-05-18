919c869736dd76f91fc2ecd1fce93c38e62bc4c5
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-05-18 08:10:28 -0300

    man: enhance build script to link command references to manned.org

--- docbuild.sh
+++ docbuild.sh
@@ -2,4 +2,4 @@
 set -e
 set -u
 
-mandoc -Thtml -Ostyle=mandoc.css extra/man/bootiso.1 > docs/index.html
+mandoc -Thtml -Ostyle=mandoc.css,toc,man=https://manned.org/%N.%S extra/man/bootiso.1 > docs/index.html
