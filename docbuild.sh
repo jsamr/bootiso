74b1add19b2c2777fcdcac1fead6d5bd04e8be1f
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-05-18 19:10:13 -0300

    docbuild: add viewport meta tag for mobiles

--- docbuild.sh
+++ docbuild.sh
@@ -2,4 +2,16 @@
 set -e
 set -u
 
-mandoc -Thtml -Ostyle=mandoc.css,toc,man=https://manned.org/%N.%S extra/man/bootiso.1 > docs/index.html
+cat > docs/index.html << HTML
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="mandoc.css" type="text/css" media="all"/>
+  <title>BOOTISO(1)</title>
+</head>
+<body>
+$(mandoc -Thtml -Otoc,fragment,man=https://manned.org/%N.%S extra/man/bootiso.1)
+</body>
+HTML
